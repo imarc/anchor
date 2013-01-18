21092eebdee96f4448e2b101dcb9c5b853928253
Kevin Hamer <kevin@imarc.net>
2013-01-18 16:41:54 -0500

    Basic structure in place

--- AnchorTest.php
+++ AnchorTest.php
@@ -1,134 +1,26 @@
 <?php
-/**
- * PHPUnit tests for Anchor.
- */
 
-class AnchorTest extends PHPUnit_Framework_TestCase {
-	public static function setUpBeforeClass() {
-		require 'Anchor.php';
+class AnchorTest extends PHPUnit_Framework_TestCase
+{
+	static public function setUpBeforeClass()
+	{
+		require_once 'Anchor.php';
+		require_once 'Route.php';
 
-		Anchor::authorize('AuthorizedController');
-		Anchor::authorize('Controller');
-
-		Anchor::add('/', 'TestController::home');
-		Anchor::add('/:class/^id/:method', '*::*');
-		Anchor::add('/:class/^id-:slug/:method', '*::*');
+		Anchor::add('/',                 'Foo::bar');
+		Anchor::add('/:section/:method', 'Home::*method');
 	}
 
-	public function testAuthorization() {
-		$this->assertInternalType('object', Anchor::call('AuthorizedController::index'));
-		$this->assertInternalType('object', Anchor::call('TestController::index'));
-		$this->assertInternalType('object', Anchor::call('TestController::allowed'));
-		$this->assertFalse(Anchor::call('UnauthorizedController::index'));
-	}
-
-	public function testResolve() {
+	public function testResolve()
+	{
 		$this->assertEquals(
-			'TestController::home',
+			'Foo::bar',
 			Anchor::resolve('/')
 		);
-		$this->assertEquals(
-			'TestController::dynamic',
-			Anchor::resolve('/test_controller/5/dynamic')
-		);
-		$this->assertEquals(
-			'TestController::dynamic',
-			Anchor::resolve('/test_controller/5-here_is_the_slug/dynamic')
-		);
-	}
 
-	public function testCall() {
-		$this->assertInternalType('object', Anchor::call('TestController::allowed'));
-		$this->assertFalse(Anchor::call('TestController::notAllowedPrivate'));
-		$this->assertFalse(Anchor::call('TestController::notAllowedStatic'));
-		$this->assertFalse(Anchor::call('TestController::__notAllowedMagic'));
-	}
-
-	public function testLink() {
 		$this->assertEquals(
-			'/',
-			Anchor::link('TestController::home')
+			'Home::bananas',
+			Anchor::resolve('/about/bananas')
 		);
-		$this->assertEquals(
-			'/test_controller/5/dynamic',
-			Anchor::link('TestController::dynamic id', 5)
-		);
-		$this->assertEquals(
-			'/test_controller/5-this_is_the_slug/dynamic',
-			Anchor::link('TestController::dynamic id slug', 5, 'this_is_the_slug')
-		);
-	}
-
-	/*public function testHooks() {
-		$data = Anchor::call('AuthorizedController::index');
-		print_r($data);
-		$this->assertEquals('ibaf', $data->test);
-	}*/
-
-	public function testFormat() {
-		$data = Anchor::call('AuthorizedController::index');
-		$this->assertEquals('AuthorizedController', $data->active_class);
-		$this->assertEquals('AuthorizedController', $data->active_short_class);
-		$this->assertEquals('AuthorizedController::index', $data->active_method);
-		$this->assertEquals('index', $data->active_short_method);
-		$this->assertEquals('authorized_controller/index', $data->active_full_path);
-		$this->assertEquals('authorized_controller', $data->active_class_path);
-
 	}
 }
-
-class UnauthorizedController {
-	public function index() {}
-}
-
-class AuthorizedController {
-	public function index($data) {
-		$data->active_class = Anchor::format('%c');
-		$data->active_short_class = Anchor::format('%C');
-		$data->active_method = Anchor::format('%m');
-		$data->active_short_method = Anchor::format('%M');
-		$data->active_full_path = Anchor::format('%p');
-		$data->active_class_path = Anchor::format('%P');
-	}
-
-	public function throwException() {
-		throw new Exception();
-	}
-
-	public function throwExtendingException() {
-		throw new ExtendingException();
-	}
-
-	public static function catchException() {
-		$data->test = 'e';
-	}
-
-	public static function init($data) {
-		$data->test = 'i';
-	}
-
-	public static function before($data) {
-		$data->test .= 'b';
-	}
-
-	public static function after($data) {
-		$data->test .= 'a';
-	}
-
-	public static function finish($data) {
-		print_r("data is now $data\n");
-		$data->test .= 'f';
-	}
-}
-
-class TestController extends AuthorizedController {
-	public function allowed() {}
-	private function notAllowedPrivate() {}
-	public static function notAllowedStatic() {}
-	public function __notAllowedMagic() {}
-
-	public function home() {}
-	public function dynamic() {}
-}
-
-class ExtendingException extends Exception {}
